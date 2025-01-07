0e3b83f1d33649df103daadd2bf1560fb0e1c87c
FrancescoSnt <francesco.santanche@chromaway.com>
2025-01-07 17:55:40 +0000

    added swap

--- src/routes/swap/+page.svelte
+++ src/routes/swap/+page.svelte
@@ -1,70 +1,39 @@
 <script lang="ts">
-	import Settings from '$lib/components/settings.svelte';
-	import Tokens from '$lib/components/tokens.svelte';
+	import Alert from '$lib/components/swap/alerts.svelte';
 	import SwapBox from '$lib/sections/Swap/SwapBox.svelte';
-	import type { TokenInfo } from '$lib/types';
-	import { ASSET_TYPE_FT4, createAmount } from '@chromia/ft4';
-	import { Buffer } from 'buffer';
+	import type { AlertType } from '$lib/types';
 	import { onMount } from 'svelte';
 
-	let isTokensHidden = $state(true);
-	let isSettingsHidden = $state(true);
-	let token1: TokenInfo | undefined = $state(undefined);
-	let token2: TokenInfo | undefined = $state(undefined);
+	let alerts: AlertType[] = $state([]);
+	let lastAlertId = 0;
 
-	function switchTokens() {
-		let t = token1;
-		token1 = token2;
-		token2 = t;
+	function createAlert(isError: boolean, text: string, link = "") {
+		const a = { id: ++lastAlertId, isError, text, ttl: 100, link };
+		alerts.push(a);
+		if (isError) throw text;
 	}
-
-	function close() {
-		isTokensHidden = true;
-	}
-	function openTokens() {
-		isTokensHidden = false;
+	function killAlert(id: number) {
+		const idx = alerts.findIndex((a) => a.id === id);
+		if (idx !== -1) {
+			alerts = alerts.slice(0, idx).concat(alerts.slice(idx + 1));
+		}
 	}
 
-	function closeSettings() {
-		isSettingsHidden = true;
-	}
-	function openSettings() {
-		isSettingsHidden = false;
+	function success(message: string, link: string) {
+		createAlert(false, message, link)
 	}
 
-	onMount(() => {
-		token1 = {
-			asset: {
-				name: 'Ethereum',
-				id: Buffer.alloc(32),
-				symbol: 'ETH',
-				decimals: 18,
-				blockchainRid: Buffer.alloc(32),
-				iconUrl:
-					'https://ethereum.org/_next/image/?url=%2F_next%2Fstatic%2Fmedia%2Feth-diamond-glyph.3cd60daa.png&w=828&q=75',
-				type: ASSET_TYPE_FT4,
-				supply: 0n
-			},
-			amountOwned: createAmount(1.02, 18),
-			amountToExchange: createAmount(1.015, 18)
-		};
-		token2 = {
-			asset: {
-				name: 'Chroma',
-				id: Buffer.alloc(32),
-				symbol: 'CHR',
-				decimals: 6,
-				blockchainRid: Buffer.alloc(32),
-				iconUrl: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3978.png',
-				type: ASSET_TYPE_FT4,
-				supply: 0n
-			},
-			amountOwned: createAmount(1216.792, 6),
-			amountToExchange: createAmount(1000, 6)
-		};
+	onMount(async () => {
+		window.addEventListener("unhandledrejection", (e) => {
+			const message = e.reason ?? e;
+			createAlert(true, message)
+		});
 	});
 </script>
 
-<SwapBox {openTokens} {openSettings} {token1} {token2} {switchTokens} />
-<Tokens {close} isHidden={isTokensHidden} />
-<Settings close={closeSettings} isHidden={isSettingsHidden} />
+<SwapBox {success}/>
+<div class="absolute left-0 bottom-0 bg-transparent flex flex-col-reverse p-4">
+	{#each alerts as a (a.id)}
+		<Alert {...a} kill={() => killAlert(a.id)} />
+	{/each}
+</div>
